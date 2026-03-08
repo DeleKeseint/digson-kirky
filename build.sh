0e20bf534e10d0170a84f12190cce08f6786f0d9
points14514 <3790010758@qq.com>
2026-03-08 22:01:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,24 +2,24 @@ timg Screenshot_2026_0220_173156.png
 echo "digson toolset 0.6"
 echo "Build give(chmod) command"
 cd give
-gcc source.c -o give
+gcc source.c -o give -lm -lcurl
 cd ..
 echo "Build take(mv) command"
 cd take
-gcc take.c -o take
+gcc take.c -o take -lm -lcurl
 cd ..
 echo "Build have command"
 cd have
-gcc source.c -o have
+gcc source.c -o have -lm -lcurl
 cd ..
 echo "Build say(echo) command"
 cd say
-gcc source.c -o say
+gcc source.c -o say -lm -lcurl
 cd ..
 echo "Build see(ls) command"
 cd see
-gcc source.c -o see
+gcc source.c -o see -lm -lcurl
 cd ..
 echo "Build get(wget curl) command"
 cd get
-gcc source.c -o get
+gcc source.c -o get -lm -lcurl
