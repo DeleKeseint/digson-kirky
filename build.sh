5843deeba3d9c37f28258ed35f407a44036168b9
points14514 <3790010758@qq.com>
2026-03-08 21:56:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 timg Screenshot_2026_0220_173156.png
-echo "digson toolset 0.5"
+echo "digson toolset 0.6"
 echo "Build give(chmod) command"
 cd give
 gcc source.c -o give
@@ -19,3 +19,7 @@ cd ..
 echo "Build see(ls) command"
 cd see
 gcc source.c -o see
+cd ..
+echo "Build get(wget curl) command"
+cd get
+gcc source.c -o get
