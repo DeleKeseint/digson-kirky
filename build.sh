0bd983c784a40457ca9ae6a0ea53b59a3021cf14
points14514 <3790010758@qq.com>
2026-03-10 20:56:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,40 +3,40 @@ echo "digson toolset 1.1"
 echo "warning: If one of the compiles fails, the version is called Kirky version"
 echo "Build give(chmod) command"
 cd give
-gcc source.c -o give -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o give -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "Build take(mv) command"
 cd take
-gcc take.c -o take -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc take.c -o take -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "Build have command"
 cd have
-gcc source.c -o have -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o have -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "Build say(echo) command"
 cd say
-gcc source.c -o say -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o say -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "Build see(ls) command"
 cd see
-gcc source.c -o see -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o see -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "Build get(wget curl) command"
 cd get
-gcc source.c -o get -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o get -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "build think(mini echo) command"
 cd think
-gcc source.c -o think -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o think -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "build work command"
 cd work
-gcc source.c -o work -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o work -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 echo "build eat(rm) command"
 cd ..
 cd eat
-gcc source.c -o eat -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o eat -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "Build setting command"
 cd setting
-gcc source.c -o setting -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -liw -lbluetooth -Wall -O2
+gcc source.c -o setting -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
