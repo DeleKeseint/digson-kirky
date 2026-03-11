f54cce7d839be290c0783cd0b38387512e3b920e
points14514 <3790010758@qq.com>
2026-03-11 21:01:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 timg Screenshot_2026_0220_173156.png
-echo "digson toolset 1.2"
+echo "digson toolset 1.3"
 echo "warning: If one of the compiles fails, the version is called Kirky version"
+echo "warning: All desktop environments in this toolset require a makefile to build applications(build command:cd status-nast && make)"
 echo "Build give(chmod) command"
 cd give
 gcc source.c -o give -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
@@ -39,7 +40,7 @@ gcc source.c -o eat -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION
 cd ..
 echo "Build setting command"
 cd setting
-gcc source.c -o setting -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
+gcc source1.c -o setting -lm -lcurl -lssh -D LIBSSH_VERSION_MAJOR=0 -D LIBSSH_VERSION_MINOR=9 -lncurses -lbluetooth -Wall -O2
 cd ..
 echo "Build Notification command"
 cd Notification
